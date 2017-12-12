b567461e75215e04b9e19dd522f48768ff8556d6
Mario Nieto <mnp18@alu.ua.es>
2017-12-12 19:01:19 +0100

    added main file

---
+++ NFAtoDFA.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 29 12:31:51 2017
+
+@author: mario
+"""
+
+import numpy as np
+
+class reader:
+    def read(name):
+        pass
+    def check():
+        pass
+
+def main():
+    
+    pass
+
+if __name__ == "__main__":
+    main()
